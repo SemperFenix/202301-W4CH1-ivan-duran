570c508aecde9333db1a8913d9a1f909a0aa4285
SemperFenix <dureivan@gmail.com>
2023-02-06 19:58:29 +0100

    Update app to call gentleman

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,32 @@
-import "./App.css";
+import { Button } from "../button/button";
+import { Gentleman } from "../gentleman/gentleman";
+import { Info } from "../info/info";
+import { MOCK_CASPOSOS } from "../../mocks/mock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
+        <Button />
+      </header>
+      <main className="main">
+        <ul className="gentlemen">
+          {MOCK_CASPOSOS.map((item) => (
+            <Gentleman
+              id={item.id}
+              name={item.name}
+              status={item.status}
+              profession={item.profession}
+              twitter={item.twitter}
+              picture={item.picture}
+              alternativeText={item.alternativeText}
+              selected={item.selected}
+            ></Gentleman>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
