f2eabe1a372c844be5e73da44af5fa45fb21ddbe
SemperFenix <dureivan@gmail.com>
2023-02-06 21:23:55 +0100

    Update app and button for readability

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -5,11 +5,8 @@ import { Info } from "../info/info";
 function App() {
   return (
     <>
-      <header className="main-header">
-        <h1 className="main-title">The pointing gentlemen</h1>
-        <Button />
-      </header>
-      <Info></Info>
+      <Button />
+      <Info />
     </>
   );
 }
