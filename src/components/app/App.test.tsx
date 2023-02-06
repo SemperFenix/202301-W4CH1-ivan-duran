6057a5dc0c1d636b574fe755f2ea2715fdfd930d
SemperFenix <dureivan@gmail.com>
2023-02-06 21:24:05 +0100

    Add app test

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Given the App component", () => {
+  describe("When called upon it", () => {
+    test("renders learn react link", () => {
+      render(<App />);
+      const linkElement = screen.getByText(/the pointing gentlemen/i);
+      expect(linkElement).toBeInTheDocument();
+      const role = screen.getAllByRole("list");
+      expect(role.length).toBe(4);
+    });
+  });
 });
